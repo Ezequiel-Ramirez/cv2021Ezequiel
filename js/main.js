6f8e947467b0bd58f44e1e8dc04eaa0066a7add3
Ezequiel Ramirez <ezequielram@gmail.com>
2021-09-08 17:34:14 -0300

    progress bar

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ $(document).ready(function(){
         $("nav ul").toggleClass("active-menu")
     })
 })
+
+/* progress-bar */
+
+let progressbar = document.querySelector(".progress-bar");
+function ScrollProgressBar(){
+    let scrollTop = document.documentElement.scrollTop;
+    let scrollHeight = document.documentElement.scrollHeight;
+    let clientHeight = document.documentElement.clientHeight;
+
+    let windowHeight = scrollHeight - clientHeight;
+    let porcentaje = scrollTop / windowHeight * 100;
+
+    progressbar.style.width = porcentaje + "%"
+}
+
+window.addEventListener("scroll", ScrollProgressBar);
+
